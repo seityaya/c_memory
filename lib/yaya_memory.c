7c0e713c16bc3b5828f2d28c0616ac6368f08f66
yaya <seityaya@ukr.net>
2023-01-27 00:10:34 +0300

    Copyright 2023

--- lib/yaya_memory.c
+++ lib/yaya_memory.c
@@ -3,7 +3,7 @@
 //Creation Date          : 2022.12
 //License Link           : https://spdx.org/licenses/LGPL-2.1-or-later.html
 //SPDX-License-Identifier: LGPL-2.1-or-later
-//Copyright © 2022-2022 Seityagiya Terlekchi. All rights reserved.
+//Copyright © 2022-2023 Seityagiya Terlekchi. All rights reserved.
 
 #include "inttypes.h"
 #include "malloc.h"
